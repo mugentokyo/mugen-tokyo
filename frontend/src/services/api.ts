1aad955924ee4c077b9dc13321ea0221f14a1fc7
mugentokyo <dummyakun678@gmail.com>
2026-01-07 23:28:06 +0700

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,9 @@
 // src/services/api.ts
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
+
+export default api;

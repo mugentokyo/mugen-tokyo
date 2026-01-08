f9fbaa8c6dc5695593349395d1837ff2829c5558
mugentokyo <dummyakun678@gmail.com>
2026-01-08 09:57:14 +0700

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,14 @@ import user from "./routes/users";
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://mugen-tokyo.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // routes
